83a2108a5021ba9e655f8d64c196a54e37c0ba03
Facundo Ferrara <facuferrarahotmail.com>
2020-05-26 22:15:31 -0300

    Revert "Merge branch 'master' of https://github.com/B1max/interfacesDeUsuario2020"
    This reverts commit ca190100932a255503b3ea14e47a57a736888437, reversing
    changes made to bb460c78b18211a86801700958b9b994f54744a8.

--- clase06/js/login.js
+++ clase06/js/login.js
@@ -1,8 +1,4 @@
 const botonLogear = document.getElementById("BotonLogear");
-const itemsMenu = document.getElementById("elementoMenu");
-itemsMenu.style.visibility = "hidden";
 botonLogear.addEventListener("click",function(){
-    itemsMenu.style.display = ((e.style.display!='none') ? 'none' : 'block');
-
-    // e.style.display = ((e.style.display!='none') ? 'none' : 'block');
+    document.getElementById("elementoMenu").style.opacity = 100;
 })
