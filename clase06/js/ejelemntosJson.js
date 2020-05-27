625bc8f6dd80fd01cd710c34e8dadd90e28357fb
Facundo Ferrara <facuferrarahotmail.com>
2020-05-27 16:16:34 -0300

    falta importar..

---
+++ clase06/js/ejelemntosJson.js
@@ -0,0 +1,22 @@
+[
+  {
+    "checkbox": "Molecule Man",
+    "fecha": [fecha("dd/mm/yyyy")],
+    "descripcion": [text(100)],
+    "estado": [
+      "Radiation resistance",
+      "Turning tiny",
+      "Radiation blast"
+    ]
+  },
+  // {
+  //   "name": "Madame Uppercut",
+  //   "age": 39,
+  //   "secretIdentity": "Jane Wilson",
+  //   "powers": [
+  //     "Million tonne punch",
+  //     "Damage resistance",
+  //     "Superhuman reflexes"
+  //   ]
+  // }
+]
