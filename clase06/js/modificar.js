bf5846541cf39cd48e494a30bebeb8f3ba647713
DarioSabatini <beunomax@yahoo.com.ar>
2020-06-05 16:47:34 -0300

    modificar en camino

--- clase06/js/modificar.js
+++ clase06/js/modificar.js
@@ -1,9 +1,11 @@
-let htmlPantallaModificar = ["<div id='NUEVA-RECTANGULO-FONDO' class='NUEVA-RECTANGULO-FONDO'>"+
-"<P id='NUEVA-TXT-TITULO' class='NUEVA-TXT-TITULO'>NUEVA SOLICITUD</P>"+
+function htmlPantallaModificar(fecha,desc,estado){ 
+    return ["<div id='NUEVA-RECTANGULO-FONDO' class='NUEVA-RECTANGULO-FONDO'>"+
+"<P id='NUEVA-TXT-TITULO' class='NUEVA-TXT-TITULO'>MODIFICAR SOLICITUD</P>"+
 "<P  id='NUEVA-TXT-FECHA' class='NUEVA-TXT-FECHA' required>FECHA:</P>"+
 "<INPUT type='date'  id='NUEVA-INPUT-FECHA' class='NUEVA-INPUT-FECHA' > </INPUT>"+
 "<P  id='NUEVA-TXT-DESCRIPCION' class='NUEVA-TXT-DESCRIPCION'>DESCRIPCÍON:</P>"+
-"<textarea  id='NUEVA-TXTTAREA-DESCRIPCION' class='NUEVA-TXTTAREA-DESCRIPCION' cols='30' rows='10' maxlength='100' required></textarea>"+
+"<textarea  id='NUEVA-TXTTAREA-DESCRIPCION' vclass='NUEVA-TXTTAREA-DESCRIPCION'" +
+"cols='30' rows='10' maxlength='100' required></textarea>"+
 "<p  id='NUEVA-TXT-ESTADO' class='NUEVA-TXT-ESTADO'>ESTADO:</p>"+
 "<select  id='NUEVA-INPUTLIST-ESTADO' class='NUEVA-INPUTLIST-ESTADO' required>"+
 "    <option value='Abierta'>Abierta</option>"+
@@ -13,3 +15,17 @@ let htmlPantallaModificar = ["<div id='NUEVA-RECTANGULO-FONDO' class='NUEVA-RECT
 "<div  id='NUEVA-RECTANGULO-BTN-CANCELAR' class='NUEVA-RECTANGULO-BTN-CANCELAR' >CANCELAR</div>"+
 "<div  id='NUEVA-RECTANGULO-BTN-ACEPTAR' class='NUEVA-RECTANGULO-BTN-ACEPTAR' >ACEPTAR</div>"+
 "</div>"];
+};
+
+function dibujarPantallaModificar(){
+    const inicial = document.getElementById("ultimo").parentNode;
+
+    console.log("dibujando pantalla nuevo");
+    htmlPantallaModificar("25/5/85","teeesssstttt","Aceptado").forEach(async function(item){
+        console.log("agregando items de pantalla nuevo");
+        await inicial.insertAdjacentHTML("beforeEnd",item);
+    })
+    document.getElementById("NUEVA-TXT-FECHA").defaultValue = "1985/5/5";
+    // document.getElementById("NUEVA-TXTTAREA-DESCRIPCION").value = "teeesssstttt";
+    document.getElementById("NUEVA-INPUTLIST-ESTADO").value = "En progreso";
+}
