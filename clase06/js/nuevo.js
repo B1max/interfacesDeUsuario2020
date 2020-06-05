45d228f39e376c5ae12ce8bdb4a4a69a0c8784ce
DarioSabatini <beunomax@yahoo.com.ar>
2020-06-05 09:18:08 -0300

    nuevo casi terminado

--- clase06/js/nuevo.js
+++ clase06/js/nuevo.js
@@ -1,18 +1,19 @@
-let htmlPantallaNuevo = "<div class='NUEVA-RECTANGULO-FONDO'>"+
-"<P class='NUEVA-TXT-TITULO'>NUEVA SOLICITUD</P>"+
-"<P class='NUEVA-TXT-FECHA'>FECHA:</P>"+
-"<INPUT type='date' class='NUEVA-INPUT-FECHA'> </INPUT>"+
-"<P class='NUEVA-TXT-DESCRIPCION'>DESCRIPCÍON:</P>"+
-"<textarea class='NUEVA-TXTTAREA-DESCRIPCION' cols='30' rows='10' maxlength='100'></textarea>"+
-"<p class='NUEVA-TXT-ESTADO'>ESTADO:</p>"+
-"<select class='NUEVA-INPUTLIST-ESTADO'>"+
+let PANTALLA_NUEVO = false;
+let htmlPantallaNuevo = ["<div id='NUEVA-RECTANGULO-FONDO' class='NUEVA-RECTANGULO-FONDO'>"+
+"<P id='NUEVA-TXT-TITULO' class='NUEVA-TXT-TITULO'>NUEVA SOLICITUD</P>"+
+"<P  id='NUEVA-TXT-FECHA' class='NUEVA-TXT-FECHA'>FECHA:</P>"+
+"<INPUT type='date'  id='NUEVA-INPUT-FECHA' class='NUEVA-INPUT-FECHA'> </INPUT>"+
+"<P  id='NUEVA-TXT-DESCRIPCION' class='NUEVA-TXT-DESCRIPCION'>DESCRIPCÍON:</P>"+
+"<textarea  id='NUEVA-TXTTAREA-DESCRIPCION' class='NUEVA-TXTTAREA-DESCRIPCION' cols='30' rows='10' maxlength='100'></textarea>"+
+"<p  id='NUEVA-TXT-ESTADO' class='NUEVA-TXT-ESTADO'>ESTADO:</p>"+
+"<select  id='NUEVA-INPUTLIST-ESTADO' class='NUEVA-INPUTLIST-ESTADO'>"+
 "    <option value='Abierta'>Abierta</option>"+
 "    <option value='En progreso'>En progreso</option>"+
 "    <option value='Cerrada'>Cerrada</option>"+
 "</select>"+
-"<div class='NUEVA-RECTANGULO-BTN-CANCELAR' >CANCELAR</div>"+
-"<div class='NUEVA-RECTANGULO-BTN-ACEPTAR' >ACEPTAR</div>"+
-"</div>";
+"<div  id='NUEVA-RECTANGULO-BTN-CANCELAR' class='NUEVA-RECTANGULO-BTN-CANCELAR' >CANCELAR</div>"+
+"<div  id='NUEVA-RECTANGULO-BTN-ACEPTAR' class='NUEVA-RECTANGULO-BTN-ACEPTAR' >ACEPTAR</div>"+
+"</div>"];
 
 async function CARGAR_PANTALLA_NUEVO(){
     await dibujarPantallaNuevo();
@@ -27,20 +28,13 @@ function dibujarPantallaNuevo(){
         await inicial.insertAdjacentHTML("beforeEnd",item);
     })
 }
-function borrarPantallaNuevo(){
+
+async function BORRAR_PANTALLA_NUEVO(){
     document.getElementById("NUEVA-RECTANGULO-FONDO").remove();
-    document.getElementById("NUEVA-TXT-TITULO").remove();
-    document.getElementById("NUEVA-TXT-FECHA").remove();
-    document.getElementById("NUEVA-INPUT-FECHA").remove();
-    document.getElementById("NUEVA-TXT-DESCRIPCION").remove();
-    document.getElementById("NUEVA-TXTTAREA-DESCRIPCION'").remove();
-    document.getElementById("NUEVA-TXT-ESTADO").remove();
-    document.getElementById("NUEVA-INPUTLIST-ESTADO").remove();
-    document.getElementById("NUEVA-RECTANGULO-BTN-CANCELAR").remove();
-    document.getElementById("NUEVA-RECTANGULO-BTN-ACEPTAR").remove();
 }
 
 function EVENTOS_PANTALLA_NUEVO(){
     document.getElementById("NUEVA-RECTANGULO-BTN-ACEPTAR");
     document.getElementById("NUEVA-RECTANGULO-BTN-CANCELAR");
 }
+
