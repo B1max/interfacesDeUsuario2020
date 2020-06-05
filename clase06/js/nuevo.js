cc2d60b5f80a9ce5dabc61cbae1c0e155e50f139
DarioSabatini <beunomax@yahoo.com.ar>
2020-06-04 23:50:37 -0300

    pantalla nuevo, en progreso

---
+++ clase06/js/nuevo.js
@@ -0,0 +1,15 @@
+let htmlPantallaNuevo = "<div class="NUEVA-RECTANGULO-FONDO">"
+            <P class="NUEVA-TXT-TITULO">NUEVA SOLICITUD</P>
+            <P class="NUEVA-TXT-FECHA">FECHA:</P>
+            <INPUT type="date" class="NUEVA-INPUT-FECHA"> </INPUT>
+            <P class="NUEVA-TXT-DESCRIPCION">DESCRIPCÍON:</P>
+            <textarea class="NUEVA-TXTTAREA-DESCRIPCION" cols="30" rows="10" maxlength="100"></textarea>
+            <p class="NUEVA-TXT-ESTADO">ESTADO:</p>
+            <select class="NUEVA-INPUTLIST-ESTADO">
+                <option value="Abierta">Abierta</option>
+                <option value="En progreso">En progreso</option>
+                <option value="Cerrada">Cerrada</option>
+            </select>
+            <div class="NUEVA-RECTANGULO-BTN-CANCELAR" >CANCELAR</div>
+            <div class="NUEVA-RECTANGULO-BTN-ACEPTAR" >ACEPTAR</div>
+        </div>";
